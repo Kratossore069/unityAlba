df6a6772c33abb4228bb5ccf31a70cb39390410c
kratossore069 <victorgabrielarostegui@gmail.com>
2021-08-24 12:21:21 +0100

    Tengo problemas para entender los scripts

---
+++ AlbaMayoJuego/Assets/Scripts/Personaje.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    GameObject algo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(algo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
