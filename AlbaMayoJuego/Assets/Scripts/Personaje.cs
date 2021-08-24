4ed4b04665b4bef4f8d45eca5684d31f3bf2055c
kratossore069 <victorgabrielarostegui@gmail.com>
2021-08-24 16:17:28 +0100

    No he hecho nada importante, sólo el personaje maś grande

--- AlbaMayoJuego/Assets/Scripts/Personaje.cs
+++ AlbaMayoJuego/Assets/Scripts/Personaje.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Personaje : MonoBehaviour
 {
-    GameObject algo;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(algo);
+        
     }
 
     // Update is called once per frame
